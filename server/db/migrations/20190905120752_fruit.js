c980c5a61fd46f195ff33d1b60834f46bf371fb2
Chris Hofsteede <chris@hofsteede.com>
2024-09-03 11:52:11 +1200

    commented out all fruit stuff, renamed index.html, npm i

--- server/db/migrations/20190905120752_fruit.js
+++ server/db/migrations/20190905120752_fruit.js
@@ -2,13 +2,13 @@
  * @param {import('knex').Knex} knex
  */
 export async function up(knex) {
-  return knex.schema.createTable('fruit', (table) => {
-    table.increments('id')
-    table.string('name')
-    table.string('owner').defaultTo(null)
-  })
-}
+//   return knex.schema.createTable('fruit', (table) => {
+//     table.increments('id')
+//     table.string('name')
+//     table.string('owner').defaultTo(null)
+//   })
+// }
 
-export async function down(knex) {
-  return knex.schema.dropTable('fruit')
-}
+// export async function down(knex) {
+//   return knex.schema.dropTable('fruit')
+// }
