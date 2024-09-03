9e5336cb7731186d7bec58ec39dc1457b0ef0ec2
Chris Hofsteede <chris@hofsteede.com>
2024-09-03 16:28:59 +1200

    implement image list and hover-over effect

--- client/components/Explorer.tsx
+++ client/components/Explorer.tsx
@@ -1,11 +1,44 @@
+import useExplore from '../hooks/use-explore.ts'
+import LoadingIndicator from './LoadingIndicator'
+
 export default function Explorer() {
+  const { isPending, isError, data } = useExplore()
+
+  // console.log('this is explorer', explorer)
+
+  if (isPending) {
+    return (
+      <>
+        <LoadingIndicator />
+      </>
+    )
+  }
+
+  if (isError) {
+    return <>Oops</>
+  }
+
+  // console.log(' I got the data', data)
+
   return (
     <>
-      <h2>Explorer</h2>
-      <p>
-        This is where you can see random options, or also the output of your own
-        search.
-      </p>
+      <div className="location-grid">
+        {data?.map((place) => (
+          <div key={place.id} className="location-card">
+            <img
+              src={place.imageUrl}
+              alt={place.name}
+              className="location-image"
+            ></img>
+            <div className="location-overlay">
+              <div className="location-info">
+                <h3>{place.name}</h3>
+                <p>{place.userRating}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
