e379604744462a1d5905da289c679370c4ba9a22
Lei Zheng <zhenglei530@gmail.com>
2024-09-07 12:41:17 +1200

    fixed optional  parameters can not be binded in object

--- client/apis/trip.ts
+++ client/apis/trip.ts
@@ -13,24 +13,24 @@ export async function getAllTrips(Auth0ID?:string) {
     }
 }
 
-export async function addTrip({trip_name,Auth0ID?,
+export async function addTrip(trip_name,Auth0ID?,
   
   start_date?,
   end_date?
-  }) {
+  ) {
  
   try {
-    const res = await request.post(rootUrl + '/trips').send({trip_name,Auth0ID?,
+    const res = await request.post(rootUrl + '/trips').send({trip_name,Auth0ID,
   
-      start_date?,
-      end_date?
+      start_date,
+      end_date
       }) //need more data in .send()
     return res.body
   } catch (error) {
     throw new Error('Sorry,can not find anything')
   }
 }
-export async function delTrip(id:Number) {
+export async function delTrip(id:number) {
   if(isNaN(Number(id))){throw new Error('Sorry,can not find anything')}
   try {
     const res = await request.delete(rootUrl + `trips/${id}`) //need more data in .send()
--- client/hooks/useTrip.ts
+++ client/hooks/useTrip.ts
@@ -2,8 +2,8 @@ import { useQuery } from '@tanstack/react-query'
 import { useMutation } from '@tanstack/react-query'
 import { useQueryClient } from '@tanstack/react-query'
 import * as tripsFn from '../apis/trip'
-import { SearchData } from '../../models/search'
-export default function useTrips({trip_name,Auth0ID?,start_date?,end_date?,id?}) {
+
+export default function useTrips(trip_name,Auth0ID?,start_date?,end_date?,id?) {
   const queryClient = useQueryClient()
   const allTrips = useQuery(
     {
@@ -13,9 +13,9 @@ export default function useTrips({trip_name,Auth0ID?,start_date?,end_date?,id?})
     // may be can use data or location for queryKey
   )
   const addTrip = useMutation({
-    mutationFn: ()=>tripsFn.addTrip({trip_name,Auth0ID?,start_date?,
-      end_date?
-      }),
+    mutationFn: ()=>tripsFn.addTrip(trip_name,Auth0ID,start_date,
+      end_date
+      ),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['trips'] })
     }
